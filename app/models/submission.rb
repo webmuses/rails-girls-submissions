01a0de67cb5e83627c4d19e7ad684fa4e10801a2
majhogee <jo.wojtowicz@gmail.com>
2016-04-19 16:22:21 +0200

    Add presence validators and radio button for OS

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,2 +1,19 @@
 class Submission < ActiveRecord::Base
+  validates :full_name, presence: true
+  validates :age, presence: true
+  validates :email, presence: true
+  validates :codeacademy_username, presence: true
+  validates :description, presence: true
+  validates :html, presence: true
+  validates :css, presence: true
+  validates :js, presence: true
+  validates :ror, presence: true
+  validates :db, presence: true
+  validates :programming_others, presence: true
+  validates :english, presence: true
+  validates :operating_system, presence: true
+  validates :first_time, presence: true
+  validates :goals, presence: true
+
+
 end
