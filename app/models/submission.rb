8742bbbb5521bcc5f326ce0028ac8cb89e5fe21d
majhogee <jo.wojtowicz@gmail.com>
2016-04-19 16:41:06 +0200

    Add age validator (between 0 and 110) and email format validator

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,7 +1,8 @@
 class Submission < ActiveRecord::Base
   validates :full_name, presence: true
-  validates :age, presence: true
+  validates :age, presence: true, numericality: {greater_than: 0, less_than: 110}
   validates :email, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :codeacademy_username, presence: true
   validates :description, presence: true
   validates :html, presence: true
