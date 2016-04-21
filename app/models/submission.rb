9a00eaf387af300f29fc773ce6b4f257df9fcc0a
majhogee <jo.wojtowicz@gmail.com>
2016-04-21 16:29:29 +0200

    Reorganizing submission validators

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,20 +1,8 @@
 class Submission < ActiveRecord::Base
-  validates :full_name, presence: true
-  validates :age, presence: true, numericality: {greater_than: 0, less_than: 110}
-  validates :email, presence: true
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  validates :codeacademy_username, presence: true
-  validates :description, presence: true
-  validates :html, presence: true
-  validates :css, presence: true
-  validates :js, presence: true
-  validates :ror, presence: true
-  validates :db, presence: true
-  validates :programming_others, presence: true
-  validates :english, presence: true
-  validates :operating_system, presence: true
-  validates :first_time, presence: true
-  validates :goals, presence: true
+  validates :full_name, :age, :email, :codeacademy_username, :description, :html, :css, :js, :ror, :db,
+            :programming_others, :english, :operating_system, :first_time, :goals, presence: true
 
+  validates :age, numericality: {greater_than: 0, less_than: 110}
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
 end
