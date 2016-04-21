c63223522356094f3fbc5f566fdc329f029eaa49
majhogee <jo.wojtowicz@gmail.com>
2016-04-21 14:30:37 +0200

    [registration-form] Add test for create new submission with valid parameters

--- spec/controllers/submissions_controller_spec.rb
+++ spec/controllers/submissions_controller_spec.rb
@@ -1,7 +1,31 @@
-# require "rails_helper"
-#
-# RSpec.describe SubmissionsController, type: :controller do
-#   describe "POST #create" do
-#
-#   end
-# end
+require "rails_helper"
+
+RSpec.describe SubmissionsController, type: :controller do
+  describe "GET #new" do
+
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+  end
+
+  describe "POST #create" do
+    context "with vaild submission parameters" do
+
+      subject {post :create, submission: {full_name: "Jan Kowalski", email: "kowalski@domain.com", age: 20, codeacademy_username: "jan_kowalski",
+      description: "I want to be a programmer!/n I want to be a programmer!", html: :used, css: :used, js: :used, ror: :used, db: :used,
+      programming_others: :used, english: :basic, operating_system: :mac, first_time: true, goals: "Job as a developer/n", problems: "Allergic to dairy"}}
+
+      it "redirects to new submission" do
+        expect(subject).to redirect_to(assigns(:submission))
+      end
+
+      it "saves the new submission" do
+        expect{subject}.to change(Submission, :count).by(1)
+      end
+    end
+  end
+
+
+end
