ab71fd832d758393cda5a77b508ff3b7d5ddf9c9
majhogee <jo.wojtowicz@gmail.com>
2016-04-26 17:24:54 +0200

    Generate devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :submissions
+  # devise_for :users, skip: [:password], :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # resources :submissions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
