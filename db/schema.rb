03d03fb563d7a9a66a0a41720c82be1cabf922c9
majhogee <jo.wojtowicz@gmail.com>
2016-04-26 11:41:43 +0200

    Add model User, ignore secrets.yml, move api keys to secrets.yml

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425120114) do
+ActiveRecord::Schema.define(version: 20160425132959) do
 
   create_table "submissions", force: :cascade do |t|
     t.string   "full_name"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20160425120114) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
