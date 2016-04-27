fe3a15c9687cf66a9b6046d433108434f6cb5a07
majhogee <jo.wojtowicz@gmail.com>
2016-04-27 13:58:40 +0200

    Add spaces in Submission age validator

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,7 +2,7 @@ class Submission < ActiveRecord::Base
   validates :full_name, :age, :email, :codeacademy_username, :description, :html, :css, :js, :ror, :db,
             :programming_others, :english, :operating_system, :first_time, :goals, presence: true
 
-  validates :age, numericality: {greater_than: 0, less_than: 110}
+  validates :age, numericality: { greater_than: 0, less_than: 110 }
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
 end
