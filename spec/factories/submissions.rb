0cd8d88ec0239052d1da5f05edd371a13b20dc33
majhogee <jo.wojtowicz@gmail.com>
2016-04-27 13:04:05 +0200

    Use sequence in submission factory for full_name, email, codeacademy_username

--- spec/factories/submissions.rb
+++ spec/factories/submissions.rb
@@ -1,14 +1,14 @@
 FactoryGirl.define do
   factory :submission do
-    full_name "Kuba Kubowski"
-    email "kuba.kubowski@domain.com"
+    sequence(:full_name) { |n| "User#{n}" }
+    sequence(:email) { |n| "user#{n}@domain.com" }
     age 20
-    codeacademy_username "kuba_kubowski123"
-    description "I'm super nice!\nI'm super nice!\nI'm super nice!\nI'm super nice!\nI'm super nice!\nI'm super nice!\nI'm super nice!\nI'm super nice!\nI'm super nice!\nI'm super nice!\nI'm super nice!\nI'm super nice!\n"
+    sequence(:codeacademy_username) { |n| "user#{n}" }
+    description "I'm super nice!\nI'm super nice!\nI'm super nice!\n"
     english :basic
     operating_system :windows
     first_time true
-    goals "Tons of money!\nTons of money!\nTons of money!\nTons of money!\nTons of money!\nTons of money!\nTons of money!\n"
+    goals "Tons of money!\nTons of money!\nTons of money!\n"
     problems "I'm shortsighted - can't see money's a shitty goal. :c"
     html :fluent
     css :fluent
