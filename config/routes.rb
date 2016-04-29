83375186a3c2c4a76423e505c4f1e5144aade151
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-04-29 16:37:47 +0200

    Set root to login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :submissions
   # devise_for :users, skip: [:password], :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
