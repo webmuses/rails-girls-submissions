a9d042c4bf5ffe374bb0c556c89a077722b91d86
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-05-06 15:32:46 +0200

    Refactor submission show view, refactor RatesController and SubmissionRater

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,6 +1,6 @@
 class RatesController < ApplicationController
   def create
-    submission_rater = SubmissionRater.new
+    submission_rater = SubmissionRater.new(params[:rate])
     submission_rater.rate
     redirect_to :back
   end
