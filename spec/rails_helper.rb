8dc61537244ae3558e0dd188a45e161afda1a978
majhogee <jo.wojtowicz@gmail.com>
2016-05-16 16:55:10 +0200

    Move config.infer_spec_type_from_file_location! to rails_helper so it does not cause an error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,4 +56,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.infer_spec_type_from_file_location!
 end
