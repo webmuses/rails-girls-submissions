9d8631d486cef4b1ef71c29b86c4dd9d4d48654a
majhogee <jo.wojtowicz@gmail.com>
2016-05-16 11:41:17 +0200

    Change from_omniauth method to from_omniauth! using first_or_create! to generate error if user not saved

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ActiveRecord::Base
 
   has_many :rates
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  def self.from_omniauth!(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.nickname = auth.info.nickname
       user.encrypted_password = Devise.friendly_token[0,20]
     end
