ae6a80a5094fc80607a58a84c6d8725eb6d8ebd7
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-05-17 17:51:00 +0200

    Delete comments, change instance variable to local variable (remove @)

--- app/services/submission_rater.rb
+++ app/services/submission_rater.rb
@@ -2,7 +2,7 @@ class SubmissionRater
   def set_rate(value, submission_id, user_id)
     submission = Submission.find(Integer(submission_id))
     user = User.find(Integer(user_id))
-    @rate = Rate.new({ value: value, submission: submission, user: user })
-    @rate.save
+    rate = Rate.new({ value: value, submission: submission, user: user })
+    rate.save
   end
 end
