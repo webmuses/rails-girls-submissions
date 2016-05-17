f0a9e387cc6154e6cb568758b4333244cd4ab9f1
majhogee <jo.wojtowicz@gmail.com>
2016-05-17 15:13:45 +0200

    Remove not needed modules from user model, change devise create user migration, fix saving user's email and encrypted password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable
   devise :omniauthable, :omniauth_providers => [:github]
 
   has_many :rates
 
   def self.from_omniauth!(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+      user.email = auth.info.email
       user.nickname = auth.info.nickname
       user.encrypted_password = Devise.friendly_token[0,20]
     end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -243,7 +243,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :github, Rails.application.secrets.app_id, Rails.application.secrets.app_secret
+  config.omniauth :github, Rails.application.secrets.app_id, Rails.application.secrets.app_secret, scope: "user:email"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
