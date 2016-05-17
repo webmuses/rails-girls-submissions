5a5fb7a85c0ef16cd058a13e1d97620319e6b86f
majhogee <jo.wojtowicz@gmail.com>
2016-05-17 16:28:57 +0200

    Improve rating integration test

--- spec/features/rating_spec.rb
+++ spec/features/rating_spec.rb
@@ -1,32 +1,14 @@
 include Warden::Test::Helpers
 Warden.test_mode!
-#
-# describe "the rating process" do
-#   before :each do
-#     @submission = FactoryGirl.create(:submission)
-#   end
-#
-#   it "signs me in" do
-#     @user = FactoryGirl.create(:user)
-#     sign_in @user
-#
-#     visit submission_path(@submission)
-#     click_button '4'
-#     #expect(page).to have_content 'Success'
-#   end
-# end
-
 
 describe "the rating process" do
-  before :each do
-    @submission = FactoryGirl.create(:submission)
-    user = FactoryGirl.create(:user)
-    login_as(user, scope: :user)
-  end
+  let!(:submission) { FactoryGirl.create(:submission) }
+  let!(:user) { FactoryGirl.create(:user) }
 
   it "visits submission page, finds and clicks rate button" do
-    visit submission_path(@submission)
-    click_button '4'
+    login_as(user, scope: :user)
+    visit submission_path(submission)
+    expect{click_button '4'}.to change(submission.rates, :count).by(1)
   end
 end
 
