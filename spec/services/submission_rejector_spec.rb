273a75d94644eb7d060fd24632267f8afbf29f2a
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-05-25 14:09:23 +0200

    Refactor and add new tests to SubmissionRejestor's spec

--- spec/services/submission_rejector_spec.rb
+++ spec/services/submission_rejector_spec.rb
@@ -2,10 +2,35 @@ require 'rails_helper'
 
 RSpec.describe SubmissionRejector do
   describe '#reject?' do
-    it 'returns false by default' do
-      submission_rejector = SubmissionRejector.new([])
-      submission = FactoryGirl.create(:submission)
-      expect(submission_rejector.reject?(submission.id)).to equal(false)
+    subject {described_class.new(rules).reject?(submission_id)}
+
+    context "when there are no rules" do
+      let!(:rules) { [] }
+      let!(:submission_id) { FactoryGirl.create(:submission).id }
+
+      it "doesn't reject the submission" do
+        expect(subject).to equal(false)
+      end
+    end
+    
+    context "when no rules are broken" do
+      let!(:rules) { [Rules::EnglishRule.new, Rules::RorRule.new] }
+      let!(:submission_id) { FactoryGirl.create(:submission,
+         english: "fluent", ror: "heard").id }
+
+      it "doesn't reject the submission" do
+        expect(subject).to equal(false)
+      end
+    end
+
+    context "when some rules are broken" do
+      let!(:rules) { [Rules::EnglishRule.new, Rules::AgeRule.new] }
+      let!(:submission_id) { FactoryGirl.create(:submission,
+         english: "none", age: 20).id }
+
+      it "rejects the submission" do
+        expect(subject).to equal(true)
+      end
     end
   end
 end
