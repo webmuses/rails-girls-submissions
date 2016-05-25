f81f38f88366c525e02a01ff1ff5fcad8a76ed0d
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-05-25 13:12:24 +0200

    Refactor SubmissionRejector's rules and their specs

--- spec/services/rules/first_time_rule_spec.rb
+++ spec/services/rules/first_time_rule_spec.rb
@@ -5,7 +5,7 @@ describe Rules::FirstTimeRule do
     subject { described_class.new.broken?(submission) }
 
     context 'when the applicant has attended the event before' do
-      let(:submission) { FactoryGirl.create(:submission, first_time: false) }
+      let!(:submission) { FactoryGirl.create(:submission, first_time: false) }
       it { expect(subject).to equal(true) }
     end
 
