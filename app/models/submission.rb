b73a6dd19844167f94ce2a55b15e601fc55286cb
majhogee <jo.wojtowicz@gmail.com>
2016-05-27 12:15:46 +0200

    Change spelling: codeacademy to codecademy, add codecademy_status column to submissions table, add fields to submission show view

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,5 +1,5 @@
 class Submission < ActiveRecord::Base
-  validates :full_name, :age, :email, :codeacademy_username, :description, :html, :css, :js, :ror, :db,
+  validates :full_name, :age, :email, :codecademy_username, :description, :html, :css, :js, :ror, :db,
             :programming_others, :english, :operating_system, :goals, presence: true
   validates_inclusion_of :first_time, in: [true, false]
   validates :age, numericality: { greater_than: 0, less_than: 110 }
