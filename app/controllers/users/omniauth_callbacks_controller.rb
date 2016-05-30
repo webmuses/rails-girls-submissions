de37f17c6c44dd1c4d5b0f70aed216a7cd5e2032
majhogee <jo.wojtowicz@gmail.com>
2016-05-30 17:46:57 +0200

    Keep user as a local variable in OmniauthCallbacksController

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,11 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
-    @user = User.from_omniauth!(request.env["omniauth.auth"])
+    user = User.from_omniauth!(request.env["omniauth.auth"])
     authorizator = Authorizator.new
 
-    if authorizator.allowed_to_log_in?(@user)
-      sign_in_and_redirect @user
+    if authorizator.allowed_to_log_in?(user)
+      sign_in_and_redirect user
     else
       redirect_to root_path, notice: 'You are not allowed to log in'
     end
