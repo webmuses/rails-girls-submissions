ca3db81d2f695d27593423b9fff84f9832202914
majhogee <jo.wojtowicz@gmail.com>
2016-05-30 17:29:18 +0200

    Change user_allowed? method to: allowed_to_log_in?, move from User model to Authenticator service

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,10 +2,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
     @user = User.from_omniauth!(request.env["omniauth.auth"])
-    if @user.user_allowed?
+    authorizator = Authorizator.new
+
+    if authorizator.allowed_to_log_in?(@user)
       sign_in_and_redirect @user
     else
-      redirect_to root_path, notice: 'You are not allowed to see submissions for Rails Girls'
+      redirect_to root_path, notice: 'You are not allowed to log in'
     end
   end
 
