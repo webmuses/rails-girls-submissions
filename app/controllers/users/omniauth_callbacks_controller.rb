07788d4e5d461c1d6d27a910134e8e4a6985244f
majhogee <jo.wojtowicz@gmail.com>
2016-05-30 15:11:14 +0200

    Check whether user is_allowed to log in based on email list in allowed_users.yml

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
     @user = User.from_omniauth!(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    if @user.user_allowed?
+      sign_in_and_redirect @user
+    else
+      redirect_to root_path, notice: 'You are not allowed to see submissions for Rails Girls'
+    end
   end
 
   def failure
