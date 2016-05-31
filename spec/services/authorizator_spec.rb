d0827e8ce8eca0a4e1076dde169eb98dc6a069ff
majhogee <jo.wojtowicz@gmail.com>
2016-05-31 12:02:49 +0200

    Test authorizator service

---
+++ spec/services/authorizator_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Authorizator do
+  describe '#allowed_to_log_in?' do
+    let!(:test_email) { "jo.wojtowicz@gmail.com" }
+
+    subject { described_class.new.allowed_to_log_in?(user) }
+
+    context "when the user's email is in the allowed_users.yml file" do
+      let!(:user) { FactoryGirl.create(:user, email: test_email ) }
+      it { expect(subject).to equal(true) }
+    end
+
+    context "when the user's email is not in the allowed_users.yml file" do
+      let!(:user) { FactoryGirl.create(:user) }
+      it { expect(subject).to equal(false) }
+    end
+  end
+end
