753a7719a0cb60c78ab601b2c8269574f40b1e43
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-06-01 16:09:58 +0200

    Prevent user from rating the same submission more than once

--- spec/services/submission_rater_spec.rb
+++ spec/services/submission_rater_spec.rb
@@ -1,13 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe SubmissionRater do
-  describe '#set_rate' do
+  describe '#create_rate' do
     it 'creates and saves a new rate in the db' do
       submission_rater = SubmissionRater.new
       submission = FactoryGirl.create(:submission)
       user = FactoryGirl.create(:user)
-      expect(submission_rater.set_rate(1, submission.id, user.id)).to equal(true)
+      expect(submission_rater.create_rate(1, submission.id, user.id)).to equal(true)
       expect(Rate.where(value: 1, submission_id: submission.id, user_id: user.id).first).not_to be_nil
+    end
+
+    describe '#user_has_already_rated?' do
+      subject { described_class.new.user_has_already_rated?(submission.id, user.id) }
+
+      context 'when the submmission has already been rated by the user' do
+        let!(:submission) { FactoryGirl.create(:submission) }
+        let!(:user) { FactoryGirl.create(:user) }
+
+        it do
+          submission.rates << Rate.new(user_id: user.id)
+          expect(subject).to equal(true)
+        end
+      end
+
+      context 'when the submmission has not been rated by the user yet' do
+        let!(:submission) { FactoryGirl.create(:submission) }
+        let!(:user) { FactoryGirl.create(:user) }
+
+        it do
+          submission.rates << Rate.new(user_id: (user.id + 1))
+          expect(subject).to equal(false)
+        end
+      end
     end
   end
 end
