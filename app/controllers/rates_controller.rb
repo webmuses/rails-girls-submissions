df995d84e6f7fe18c0f377a2b43fb4d2a6f0f2dd
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-06-01 16:09:58 +0200

    Prevent user from rating the same submission more than once

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -2,12 +2,20 @@ class RatesController < ApplicationController
 
   def create
     submission_rater = SubmissionRater.new
-    rated = submission_rater.set_rate(params[:value], params[:submission_id], current_user.id)
 
-    if rated
-      redirect_to :back, notice: 'Submission was rated successfully'
+    value = Integer(params[:value])
+    submission_id = Integer(params[:submission_id])
+    user_id = current_user.id
+
+    if submission_rater.user_has_already_rated?(submission_id, user_id)
+      redirect_to :back, notice: 'You have already rated this submission'
     else
-      redirect_to :back, error: 'Submission was not rated'
+      rated = submission_rater.create_rate(value, submission_id, user_id)
+      if rated
+        redirect_to :back, notice: 'Submission was rated successfully'
+      else
+        redirect_to :back, error: 'Submission was not rated'
+      end
     end
   end
 end
--- app/services/submission_rater.rb
+++ app/services/submission_rater.rb
@@ -1,8 +1,15 @@
 class SubmissionRater
-  def set_rate(value, submission_id, user_id)
-    submission = Submission.find(Integer(submission_id))
-    user = User.find(Integer(user_id))
+  def create_rate(value, submission_id, user_id)
+    submission = Submission.find(submission_id)
+    user = User.find(user_id)
+
     rate = Rate.new({ value: value, submission: submission, user: user })
     rate.save
+  end
+
+  def user_has_already_rated?(submission_id, user_id)
+    submission = Submission.find(submission_id)
+
+    submission.rates.any? { |rate| rate.user_id == user_id }
   end
 end
