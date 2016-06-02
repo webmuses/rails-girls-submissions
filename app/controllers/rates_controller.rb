b719a4d7de9fc54d319ade61e1eaf23c0d93dc49
majhogee <jo.wojtowicz@gmail.com>
2016-06-02 15:01:41 +0200

    Refactor submission_rater service and split its responsibilities between two services, rate_creator and rate_checker

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,16 +1,14 @@
 class RatesController < ApplicationController
 
   def create
-    submission_rater = SubmissionRater.new
-
     value = Integer(params[:value])
     submission_id = Integer(params[:submission_id])
     user_id = current_user.id
 
-    if submission_rater.user_has_already_rated?(submission_id, user_id)
+    if RateChecker.new.user_has_already_rated?(submission_id, user_id)
       redirect_to :back, notice: 'You have already rated this submission'
     else
-      rated = submission_rater.create_rate(value, submission_id, user_id)
+      rated = RateCreator.build(value, submission_id, user_id)
       if rated
         redirect_to :back, notice: 'Submission was rated successfully'
       else
