30c37e113512167239b37cc4b0c1c57e098bc1b4
majhogee <jo.wojtowicz@gmail.com>
2016-06-02 13:16:28 +0200

    Remove print_nickname_and_value_method as not needed after refactoring, take care of printing user nickname and value directly in submissions show view

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,6 +1,6 @@
 module SubmissionsHelper
-  def print_nickname_and_value(rate)
-    user = User.find(rate.user_id)
-    user.nickname + ": " + rate.value.to_s
-  end
+  # def print_nickname_and_value(rate)
+  #   # user = User.find(rate.user_id)
+  #   "#{rate.user.nickname}: #{rate.value}"
+  # end
 end
