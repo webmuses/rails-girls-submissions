b97aa9f935d0b1c1b877a3c8aa9c28cb72a5a626
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-06-03 16:38:04 +0200

    Refactor services creating and validating rates, add flash message in case of errors

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,11 +1,13 @@
 class RatesController < ApplicationController
-
   def create
     value = Integer(params[:value])
     submission_id = Integer(params[:submission_id])
     user_id = current_user.id
 
-    rated = RateCreator.build(value, submission_id, user_id)
+    rate_creator = RateCreator.build(value, submission_id, user_id)
+    if !rate_creator.call
+      flash[:error] = rate_creator.errors
+    end
 
     redirect_to :back
   end
