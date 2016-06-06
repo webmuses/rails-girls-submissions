8d486c1595f9ecaadb9cd41ab0c8bcf21e5efb69
majhogee <jo.wojtowicz@gmail.com>
2016-06-06 12:59:21 +0200

    Create model Comment that belongs to User and Submission, create comments controller, add routes and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   devise :omniauthable, :omniauth_providers => [:github]
 
   has_many :rates
+  has_many :comments
 
   def self.from_omniauth!(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
