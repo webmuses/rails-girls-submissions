1463bcb06bb4b326f56045fb2ffb6aebe9bdd3e1
majhogee <jo.wojtowicz@gmail.com>
2016-06-07 10:34:38 +0200

    Remove not needed methods from comments controller, and respective routes and links in submission show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,11 @@
 class CommentsController < ApplicationController
   before_action :set_submission
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  def show
-  end
 
   def new
     @comment = Comment.new
   end
 
-  def edit
-  end
-
   def create
-    @submission = Submission.find(params[:submission_id])
     @comment = @submission.comments.build({ body: comment_params[:body], submission: @submission, user: current_user })
 
     if @comment.save
@@ -23,26 +15,7 @@ class CommentsController < ApplicationController
     end
   end
 
-
-  def update
-    if @comment.update(comment_params)
-      redirect_to submission_comment_path, notice: 'Comment was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-
-  def destroy
-    @comment.destroy
-    redirect_to @submission, notice: 'Comment was successfully destroyed.'
-  end
-
   private
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
-
     def set_submission
       @submission = Submission.find(params[:submission_id])
     end
