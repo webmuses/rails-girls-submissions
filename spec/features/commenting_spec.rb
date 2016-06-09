f3cbaa2ebe7cc377e21929326d38aa884f2fef88
majhogee <jo.wojtowicz@gmail.com>
2016-06-09 14:08:41 +0200

    Fix commenting spec

--- spec/features/commenting_spec.rb
+++ spec/features/commenting_spec.rb
@@ -3,13 +3,11 @@ describe "the commenting process" do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:comment_body) { 'lalala' }
 
-  it "visits submission page, creates comment, dis" do
+  it "visits submission page, creates comment, checks if comment is being displayed" do
     login_as(user, scope: :user)
     visit submission_path(submission)
     fill_in 'comment_body', with: comment_body
     click_button('Comment')
     expect(page).to have_text(comment_body)
-    expect(page).to have_text(user.nickname)
-    expect(page).to have_text(submission.created_at)
   end
 end
