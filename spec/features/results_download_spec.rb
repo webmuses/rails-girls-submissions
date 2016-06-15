3823035c581732724caf1e9587a5d52c1f7df248
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-06-15 15:21:10 +0200

    Refactor csv downloading specs to match 'Settings' -> 'Results' refactor

--- spec/features/results_download_spec.rb
+++ spec/features/results_download_spec.rb
@@ -1,25 +1,25 @@
 describe "csv downloading process" do
   let!(:user) { FactoryGirl.create(:user) }
 
-  it "visits settings page, clicks 'Accepted', checks if csv file is downloaded" do
+  it "visits results page, clicks 'Accepted', checks if csv file is downloaded" do
     login_as(user, scope: :user)
-    visit submissions_settings_path
+    visit submissions_results_path
     click_link('Accepted')
     expect(response_headers["Content-Type"]).to eq("text/csv")
     expect(response_headers["Content-Disposition"]).to eq("attachment; filename=\"accepted.csv\"")
   end
 
-  it "visits settings page, clicks 'Waitlist', checks if csv file is downloaded" do
+  it "visits results page, clicks 'Waitlist', checks if csv file is downloaded" do
     login_as(user, scope: :user)
-    visit submissions_settings_path
+    visit submissions_results_path
     click_link('Waitlist')
     expect(response_headers["Content-Type"]).to eq("text/csv")
     expect(response_headers["Content-Disposition"]).to eq("attachment; filename=\"waitlist.csv\"")
   end
 
-  it "visits settings page, clicks 'Unaccepted', checks if csv file is downloaded" do
+  it "visits results page, clicks 'Unaccepted', checks if csv file is downloaded" do
     login_as(user, scope: :user)
-    visit submissions_settings_path
+    visit submissions_results_path
     click_link('Unaccepted')
     expect(response_headers["Content-Type"]).to eq("text/csv")
     expect(response_headers["Content-Disposition"]).to eq("attachment; filename=\"unaccepted.csv\"")
