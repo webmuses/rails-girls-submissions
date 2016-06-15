7fe6575d269de845fdcc0848c3f848bac4b75e79
majhogee <jo.wojtowicz@gmail.com>
2016-06-15 09:12:50 +0200

    Remove download_accepted/waitlist/unaccepted methods from submissions controller

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -33,39 +33,6 @@ class SubmissionsController < ApplicationController
     render :settings, locals: { submissions: submissions }
   end
 
-  def download_accepted
-    submissions_accepted = Submission.select { |submission| submission.accepted? }
-
-    respond_to do |format|
-      format.csv do
-        submissions_accepted_csv = CsvGenerator.new.to_csv(submissions_accepted)
-        send_data submissions_accepted_csv
-      end
-    end
-  end
-
-  def download_waitlist
-    submissions_waitlist = Submission.select { |submission| submission.waitlist? }
-
-    respond_to do |format|
-      format.csv do
-        submissions_waitlist_csv = CsvGenerator.new.to_csv(submissions_waitlist)
-        send_data submissions_waitlist_csv
-      end
-    end
-  end
-
-  def download_unaccepted
-    submissions_unaccepted = Submission.select { |submission| submission.unaccepted? }
-
-    respond_to do |format|
-      format.csv do
-        submissions_unaccepted_csv = CsvGenerator.new.to_csv(submissions_unaccepted)
-        send_data submissions_unaccepted_csv
-      end
-    end
-  end
-
   # GET /submissions/1
   def show
     submission = Submission.find(params[:id])
