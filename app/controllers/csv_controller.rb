0062e76bdd131f798aa85dfdf4729074f2ddb567
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-06-15 10:53:57 +0200

    Get rid of `respond_to` when generating csv files

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -3,33 +3,21 @@ class CsvController < ApplicationController
   def download_accepted
     submissions_accepted = Submission.select { |submission| submission.accepted? }
 
-    respond_to do |format|
-      format.csv do
-        submissions_accepted_csv = CsvGenerator.new.to_csv(submissions_accepted)
-        send_data submissions_accepted_csv
-      end
-    end
+    submissions_accepted_csv = CsvGenerator.new.to_csv(submissions_accepted)
+    send_data submissions_accepted_csv, filename: 'accepted.csv', type: "text/csv; charset=UTF-8;"
   end
 
   def download_waitlist
     submissions_waitlist = Submission.select { |submission| submission.waitlist? }
 
-    respond_to do |format|
-      format.csv do
-        submissions_waitlist_csv = CsvGenerator.new.to_csv(submissions_waitlist)
-        send_data submissions_waitlist_csv
-      end
-    end
+    submissions_waitlist_csv = CsvGenerator.new.to_csv(submissions_waitlist)
+    send_data submissions_waitlist_csv, filename: 'waitlist.csv', type: "text/csv; charset=UTF-8;"
   end
 
   def download_unaccepted
     submissions_unaccepted = Submission.select { |submission| submission.unaccepted? }
 
-    respond_to do |format|
-      format.csv do
-        submissions_unaccepted_csv = CsvGenerator.new.to_csv(submissions_unaccepted)
-        send_data submissions_unaccepted_csv
-      end
-    end
+    submissions_unaccepted_csv = CsvGenerator.new.to_csv(submissions_unaccepted)
+    send_data submissions_unaccepted_csv, filename: 'unaccepted.csv', type: "text/csv; charset=UTF-8;"
   end
 end
