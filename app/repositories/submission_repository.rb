7b958e359cd9a4346e1c691d12decc7134842822
majhogee <jo.wojtowicz@gmail.com>
2016-06-16 16:10:18 +0200

    Refactor submission repository, split line in submission model

--- app/repositories/submission_repository.rb
+++ app/repositories/submission_repository.rb
@@ -3,37 +3,15 @@ class SubmissionRepository
   WAITLIST_THRESHOLD = 2
 
   def accepted
-    Submission.select { |submission| accepted?(submission, ACCEPTED_THRESHOLD) }
+    Submission.joins(:rates).group('submissions.id').having('avg(value) >= ?', ACCEPTED_THRESHOLD).to_a
   end
 
   def waitlist
-    Submission.select { |submission| waitlist?(submission, ACCEPTED_THRESHOLD, WAITLIST_THRESHOLD) }
+    Submission.joins(:rates).group('submissions.id').having('avg(value) < ? AND avg(value) >= ?', ACCEPTED_THRESHOLD,
+    WAITLIST_THRESHOLD).to_a
   end
 
   def unaccepted
-    Submission.select { |submission| unaccepted?(submission, WAITLIST_THRESHOLD) }
-  end
-
-  private
-
-  def accepted?(submission, accepted_threshold)
-    average_rate(submission) >= accepted_threshold
-  end
-
-  def waitlist?(submission, accepted_threshold, waitlist_threshold)
-    average_rate(submission) < accepted_threshold && average_rate(submission) >= waitlist_threshold
-  end
-
-  def unaccepted?(submission, waitlist_threshold)
-    average_rate(submission) < waitlist_threshold
-  end
-
-  def average_rate(submission)
-    rates = submission.rates
-    if rates.count == 0
-      0
-    else
-      rates.sum(:value).to_f  / rates.count
-    end
+    Submission.joins(:rates).group('submissions.id').having('avg(value) < ?', WAITLIST_THRESHOLD).to_a
   end
 end
