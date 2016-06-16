270d4cc9cca16dc4e44f4054645fad3f71acef04
majhogee <jo.wojtowicz@gmail.com>
2016-06-16 10:58:49 +0200

    Move waitlist and unaccepted methods to submission repository

--- app/repositories/submission_repository.rb
+++ app/repositories/submission_repository.rb
@@ -1,14 +1,31 @@
 class SubmissionRepository
-  ACCEPTED_THRESHOLD = Submission::REQUIRED_RATES_NUM
+  ACCEPTED_THRESHOLD = 3
+  WAITLIST_THRESHOLD = 2
 
   def accepted
     Submission.select { |submission| accepted?(submission, ACCEPTED_THRESHOLD) }
   end
 
+  def waitlist
+    Submission.select { |submission| waitlist?(submission, ACCEPTED_THRESHOLD, WAITLIST_THRESHOLD) }
+  end
+
+  def unaccepted
+    Submission.select { |submission| unaccepted?(submission, WAITLIST_THRESHOLD) }
+  end
+
   private
 
-  def accepted?(submission, threshold)
-    average_rate(submission) >= threshold
+  def accepted?(submission, accepted_threshold)
+    average_rate(submission) >= accepted_threshold
+  end
+
+  def waitlist?(submission, accepted_threshold, waitlist_threshold)
+    average_rate(submission) < accepted_threshold && average_rate(submission) >= waitlist_threshold
+  end
+
+  def unaccepted?(submission, waitlist_threshold)
+    average_rate(submission) < waitlist_threshold
   end
 
   def average_rate(submission)
