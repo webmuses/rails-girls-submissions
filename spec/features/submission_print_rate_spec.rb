33a84d5cd31372ef93da39eda00005e04e041e7d
majhogee <jo.wojtowicz@gmail.com>
2016-06-20 10:52:56 +0200

    Refactor submission pront rate spec

--- spec/features/submission_print_rate_spec.rb
+++ spec/features/submission_print_rate_spec.rb
@@ -4,13 +4,10 @@ let!(:submission) { FactoryGirl.create(:submission) }
 let!(:user) { FactoryGirl.create(:user) }
   context "when the submission has required number of rates" do
 
-    it "shows ratings in submission view" do
-      SubmissionRepository::REQUIRED_RATES_NUM.times do
-        rate = FactoryGirl.create(:rate)
-        submission.rates << rate
-      end
-      sample_rate = submission.rates.sample
+    before { submission.rates << FactoryGirl.build_list(:rate, SubmissionRepository::REQUIRED_RATES_NUM) }
 
+    it "shows ratings in submission view" do
+      sample_rate = submission.rates.sample
       login_as(user, scope: :user)
       visit submission_path(submission.id)
       expect(page).to have_text("#{sample_rate.user.nickname} #{sample_rate.value}")
@@ -19,13 +16,10 @@ let!(:user) { FactoryGirl.create(:user) }
 
   context "when the submission does not have required number of rates" do
 
-    it "does not show rates in submission view" do
-      (SubmissionRepository::REQUIRED_RATES_NUM - 1).times do
-        rate = FactoryGirl.create(:rate)
-        submission.rates << rate
-      end
-      sample_rate = submission.rates.sample
+    before { submission.rates << FactoryGirl.build_list(:rate, SubmissionRepository::REQUIRED_RATES_NUM-1) }
 
+    it "does not show rates in submission view" do
+      sample_rate = submission.rates.sample
       login_as(user, scope: :user)
       visit submission_path(submission.id)
       expect(page).not_to have_text("#{sample_rate.user.nickname} #{sample_rate.value}")
