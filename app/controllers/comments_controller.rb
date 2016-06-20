4f2760d9d2408dc3833c36ae8f150cb885e47d2c
majhogee <jo.wojtowicz@gmail.com>
2016-06-20 11:11:44 +0200

    Fix rendering submission show view when comment is not created by sending comment_presenters and rate_presenters in locals from comment create method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,14 +11,25 @@ class CommentsController < ApplicationController
       redirect_to submission_path(submission_id), notice: 'Comment was successfully created.'
     else
       submission = Submission.find(submission_id)
+      rate_presenters = create_rate_presenters(submission.rates)
+      comment_presenters = create_comment_presenters(submission.comments)
+      
       render "submissions/show", locals: { submission: submission,
-        comment: result.object, submissions_comments: submission.comments,
-        submissions_rates: submission.rates }
+        comment: result.object, comment_presenters: comment_presenters,
+        rate_presenters: rate_presenters  }
     end
   end
 
   private
     def comment_params
       params.require(:comment).permit(:body)
+    end
+
+    def create_rate_presenters(rates)
+      rates.map { |rate| RatePresenter.new(rate, rate.user) }
+    end
+
+    def create_comment_presenters(comments)
+      comments.map { |comment| CommentPresenter.new(comment, comment.user) }
     end
 end
