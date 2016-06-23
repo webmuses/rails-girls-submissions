7adf0f0906ea4ac0c4b98453795f4d2beac6f3c6
majhogee <jo.wojtowicz@gmail.com>
2016-06-23 11:56:42 +0200

    Add preparation, registration and closed period fields to settings form

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -7,13 +7,16 @@ class SettingsController < ApplicationController
   end
 
   def update
-    Setting.set(settings["accepted_threshold"], settings["waitlist_threshold"], settings["required_rates_num"])
+    Setting.set(settings["accepted_threshold"], settings["waitlist_threshold"], settings["required_rates_num"],
+      settings["beginning_of_preparation_period"], settings["beginning_of_registration_period"],
+      settings["beginning_of_closed_period"])
     redirect_to :back, notice: "Settings are updated"
   end
 
   private
 
   def settings
-    params.require(:setting).permit(:accepted_threshold, :waitlist_threshold, :required_rates_num)
+    params.require(:setting).permit(:accepted_threshold, :waitlist_threshold, :required_rates_num,
+      :beginning_of_preparation_period, :beginning_of_registration_period, :beginning_of_closed_period)
   end
 end
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -7,11 +7,15 @@ class Setting < ActiveRecord::Base
     })
   end
 
-  def self.set(accepted_threshold, waitlist_threshold, required_rates_num)
+  def self.set(accepted_threshold, waitlist_threshold, required_rates_num, beginning_of_preparation_period,
+      beginning_of_registration_period, beginning_of_closed_period)
     settings = self.get
     settings.accepted_threshold = accepted_threshold
     settings.waitlist_threshold = waitlist_threshold
     settings.required_rates_num = required_rates_num
+    settings.beginning_of_preparation_period = beginning_of_preparation_period
+    settings.beginning_of_registration_period = beginning_of_registration_period
+    settings.beginning_of_closed_period = beginning_of_closed_period
     settings.save!
   end
 end
