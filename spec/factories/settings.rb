162ee981411de0dcfd1d52b7daa5edcf721c055e
majhogee <jo.wojtowicz@gmail.com>
2016-06-23 17:37:10 +0200

    Update submissions controller spec and submissions factory

--- spec/factories/settings.rb
+++ spec/factories/settings.rb
@@ -3,5 +3,8 @@ FactoryGirl.define do
     accepted_threshold 4
     waitlist_threshold 3
     required_rates_num 2
+    beginning_of_preparation_period "2016-06-23 17:20:53 +0200"
+    beginning_of_registration_period "2016-06-24 17:20:53 +0200"
+    beginning_of_closed_period "2016-06-25 17:20:53 +0200"
   end
 end
