8b8aa9cfe59a2568d63325e625b80d4de4fde9ed
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-06-28 13:13:39 +0200

    Move average_rate logic from Submission to SubmissionPresenter, create specs

--- app/presenters/submission_presenter.rb
+++ app/presenters/submission_presenter.rb
@@ -6,7 +6,11 @@ class SubmissionPresenter < SimpleDelegator
 
   def average_rate
     submission = __getobj__
-    submission.average_rate.round(2) if submission.rated?
+
+    if submission.rated?
+      average = @rates.count == 0 ? 0 : (@rates.sum(:value).to_f / @rates.count)
+      average.round(2)
+    end
   end
 
   def rates_count
