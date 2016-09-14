070b79c59eb4adc4bff2289f480a58598a5fa30d
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2016-09-14 23:22:19 +0200

    Don't use Setting values from the factory

--- spec/presenters/footer_presenter_spec.rb
+++ spec/presenters/footer_presenter_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe FooterPresenter do
-  let!(:setting) { FactoryGirl.create(:setting) }
+  let!(:setting) { FactoryGirl.create(:setting,
+                     event_start_date: "2016-07-09",
+                     event_end_date: "2016-07-10",
+                     event_url: "railsgirls.com/lodz")
+                  }
   subject { described_class.new(setting) }
 
   it 'presents event dates' do
-    expect(subject.event_dates).to eq('16-17 April')
+    expect(subject.event_dates).to eq('9-10 July')
   end
 
   it 'presents url to event' do
-    expect(subject.event_url).to eq('railsgirls.com/krakow')
+    expect(subject.event_url).to eq('railsgirls.com/lodz')
   end
 end
