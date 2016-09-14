0975d4d2c971b783dd84efe11dec9563c9d99bb3
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2016-09-14 14:49:23 +0200

    Clean up seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Submission.destroy_all
-
-unless Rails.env.production?
-  2.times {
-    FactoryGirl.create(:submission)
-  }
-end
