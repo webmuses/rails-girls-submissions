e52272f5453e91ab2b535c01e7d3ba9ae765ccf8
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2016-09-15 21:31:55 +0200

    Add specs for #next and #previous_to_rate

--- spec/repositories/submission_repository_spec.rb
+++ spec/repositories/submission_repository_spec.rb
@@ -16,7 +16,9 @@ describe SubmissionRepository do
   let!(:waitlist_submission) { FactoryGirl.create(:waitlist_submission, :with_settings, setting_values) }
   let!(:unaccepted_not_rejected_submission) { FactoryGirl.create(:unaccepted_not_rejected_submission, :with_settings, setting_values) }
   let!(:unaccepted_rejected_submission)  { FactoryGirl.create(:unaccepted_rejected_submission, :with_settings, setting_values) }
-  let!(:to_rate_submission) { FactoryGirl.create(:to_rate_submission) }
+  let!(:to_rate_submission_1) { FactoryGirl.create(:to_rate_submission, created_at: 1.hour.ago) }
+  let!(:to_rate_submission_2) { FactoryGirl.create(:to_rate_submission) }
+  let(:to_rate_submissions) { [to_rate_submission_2, to_rate_submission_1] }
 
   describe "#accepted" do
     subject { described_class.new.accepted }
@@ -64,7 +66,43 @@ describe SubmissionRepository do
     subject { described_class.new.to_rate }
 
     it "returns submissions that are not rejected, but are not rated yet" do
-      expect(subject).to eq [to_rate_submission]
+      expect(subject).to eq to_rate_submissions
+    end
+  end
+
+  describe "#next_to_rate" do
+    context "when there is a next submission to rate" do
+      subject { described_class.new.next_to_rate(to_rate_submission_1.created_at) }
+
+        it "returns the next submission to rate" do
+          expect(subject).to eq to_rate_submission_2
+        end
+      end
+
+    context "when there are no more submissions after" do
+      subject { described_class.new.next_to_rate(to_rate_submission_2.created_at) }
+
+      it "wraps around the submissions" do
+        expect(subject).to eq to_rate_submission_1
+      end
+    end
+  end
+
+  describe "#previous_to_rate" do
+    context "when there is a previous submission to rate" do
+      subject { described_class.new.previous_to_rate(to_rate_submission_2.created_at) }
+
+        it "returns the previous submission to rate" do
+          expect(subject).to eq to_rate_submission_1
+        end
+      end
+
+    context "when there are no more submissions before" do
+      subject { described_class.new.previous_to_rate(to_rate_submission_1.created_at) }
+
+      it "wraps around the submissions" do
+        expect(subject).to eq to_rate_submission_2
+      end
     end
   end
 end
