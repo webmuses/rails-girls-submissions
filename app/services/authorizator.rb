74530683aaeb02c6e0513d5bce61ee9e99fc9130
Ania Slimak <anna.slimak@lunarlogic.io>
2016-12-27 12:48:22 +0100

    Make it possible to login all github users (for the purpose of public demo)

--- app/services/authorizator.rb
+++ app/services/authorizator.rb
@@ -4,6 +4,6 @@ class Authorizator
 
     data = YAML.load_file "config/allowed_users.yml"
     allowed_users = data["allowed_users"]
-    allowed_users.include?(user.email)
+    allowed_users == :all || allowed_users.include?(user.email)
   end
 end
